56f87d04de96c304cb30f8631d06a00ab105c91c
sameerasw <sameera.whoami@gmail.com>
2023-12-25 03:47:26 +0530

    finalizing the design for now. bookmark buttons, title text animation, unsplash + pexels api, moving the background to a div, changing the fav folder to the favorites bar

--- sketch/home.js
+++ sketch/home.js
@@ -1,13 +1,36 @@
+let background = document.getElementById('bg');
 let body = document.querySelector('body');
 let h1 = document.querySelector('h1');
 let bookmarks = document.querySelector('#bookmarks');
-let para = document.querySelector('p');
+let para = document.getElementById('title');
 let list = document.querySelector('ul');
 const numbers = "0123456789";
 let interval = null;
 
 let previousImageUrl = '';
 h1.style.opacity = 1;
+
+function bookmarkTitle() {
+    //display bookmark title on hover from data-title onto the para
+    let link = document.querySelector('a');
+    list.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('link')) {
+            para.style.scale = 1;
+            para.innerHTML = e.target.dataset.title;
+            background.style.backdropFilter = 'blur(0px) brightness(0.2)';
+            // background.style.scale = 1.1;
+            // background.style.rotate = '2deg';
+        }
+    });
+    list.addEventListener('mouseout', (e) => {
+        if (e.target.classList.contains('link')) {
+            para.style.scale = 0;
+            background.style.backdropFilter = 'blur(1px) brightness(0.5)';
+            // background.style.scale = 1;
+            // background.style.rotate = '0deg';
+        }
+    });
+}
 
 function textanimate (word) {
     let iterations = 0;
@@ -33,7 +56,7 @@ function textanimate (word) {
         })
         .join("");
         iterations += 1;
-        console.log('textanimate()');
+        // console.log('textanimate()');
         if(h1.innerText === word){
             //stop the function
             clearInterval(interval);
@@ -50,7 +73,7 @@ function getImg() {
             var image = response.urls.regular;
             //save image to local storage
             chrome.storage.local.set({'image': image}, function() {
-                console.log('saved image', image);
+                // console.log('saved image', image);
             });
         } 
     };
@@ -62,10 +85,35 @@ function randomWallpaper() {
     //get image from local storage
     chrome.storage.local.get('image', function(data) {
         let imageUrl = data.image;
-        body.style.backgroundImage = `url(${imageUrl})`;
+        background.style.backgroundImage = `url(${imageUrl})`;
     });
-    body.style.backdropFilter = 'blur(30px) brightness(0)';
-    getImg();
+    background.style.backdropFilter = 'blur(30px) brightness(0)';
+        getImg();
+    
+    //check if the both images are same and if so, use the public api to get a new image
+    if (previousImageUrl === background.style.backgroundImage) {
+        //get from pexels api
+        console.log('Using pexels api');
+        let api = 'INB1uHU6THskPr6OnPO7sqW7Yaf9QPFecSDsc8AHf0KT7Tp9zmWuK6uW';
+        let url = `https://api.pexels.com/v1/curated?per_page=1&page=${Math.floor(Math.random() * 10) + 1}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.setRequestHeader('Authorization', api);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+                let imageUrl = response.photos[0].src.original;
+                //save image to local storage
+                chrome.storage.local.set({'image': imageUrl}, function() {
+                    // console.log('saved image', imageUrl);
+                });
+            }
+        };
+        xhr.send();
+    } else {
+        previousImageUrl = background.style.backgroundImage;
+        console.log('Using unsplash api');
+    }
 }
 
 function time() {
@@ -85,11 +133,12 @@ function time() {
 function bookmarkList() {
     let icons = [];
     //get bookmarks
-    // chrome.bookmarks.getTree(function(bookmarks) {
-    //     process_bookmark(bookmarks);
+    // chrome.bookmarks.getTree(function(bookmark3) {
+    //     // process_bookmark(bookmark3);
+    //     console.log(bookmark3);
     // });
-    chrome.bookmarks.getSubTree('206', function(bookmarks) {
-        console.log(bookmarks);
+    chrome.bookmarks.getSubTree('1', function(bookmarks) {
+        // console.log(bookmarks);
         icons = fetchBookmarkIcons(bookmarks);
         process_bookmark(bookmarks,icons);
     });
@@ -106,31 +155,26 @@ function fetchBookmarkIcons(bookmarks) {
         try {
             icons.push('https://s2.googleusercontent.com/s2/favicons?domain_url=' + bookmark.url + '&sz=256');
         } catch (error) {
-            icons.push('https://s2.googleusercontent.com/s2/favicons?domain_url=' + bookmark.children[0].url + '&sz=256');
-        }
-        //if it fails to get icon, use default icon
-        if (icons[bookmarks[0].children.indexOf(bookmark)] === 'https://s2.googleusercontent.com/s2/favicons?domain_url=&sz=256') {
             icons[bookmarks[0].children.indexOf(bookmark)] = 'https://images.vexels.com/media/users/3/223055/isolated/lists/eb3fcec56c95c2eb7ded9201e51550a2-bookmark-icon-flat.png';
         }
     });
-    console.log(icons);
+    // console.log(icons);
     return icons;
 }
 
 
 function process_bookmark(bookmarks,icons) {
-    //process bookmarks - get each child and add to list
-    // para.innerHTML = bookmarks[0].title;
     list.innerHTML = bookmarks[0].children.map(bookmark => {
-        return `<li><a href="${bookmark.url}"><img src="${icons[bookmarks[0].children.indexOf(bookmark)]}"><p>${bookmark.title}</p></a></li>`;
+        return `<li data-title="${bookmark.title}"><a href="${bookmark.url}" data-title="${bookmark.title}" class="link"><img src="${icons[bookmarks[0].children.indexOf(bookmark)]}" class="link" data-title="${bookmark.title}"></a></li>`;
     }
     ).join('');
 }
 
 function addBrightness() {
-    body.style.transition = 'all 1s ease-in-out';
-    body.style.backdropFilter = 'blur(0px) brightness(0.5)';
-    // body.style.backdropFilter = 'blur(0px) brightness(0.8)';
+    bookmarks.style.scale = 1;
+    background.style.transition = 'all 1s ease-in-out';
+    background.style.filter = 'blur(0px) brightness(0.5)';
+    // background.style.backdropFilter = 'blur(0px) brightness(0.8)';
 }
 
 function updateTime() {
@@ -139,14 +183,22 @@ function updateTime() {
     h1.innerHTML = time();
 }
 
-bookmarkList();
 
-//wait for page to load
-randomWallpaper();
-setTimeout(addBrightness, 500);
 
-console.log('randomWallpaper()');
-//update time once 5 seconds
-// textanimate('00:00');
-textanimate(time());
-setInterval(updateTime , 5000);
+//start the script after the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    bookmarkList();
+
+    //wait for page to load
+    randomWallpaper();
+    setTimeout(addBrightness, 500);
+    
+    // console.log('randomWallpaper()');
+    //update time once 5 seconds
+    // textanimate('00:00');
+    textanimate(time());
+    setInterval(updateTime , 5000);
+    
+    bookmarkTitle();
+
+});
