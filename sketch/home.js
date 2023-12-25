8cb17b34afaffe38cedd3942f22776318af71186
sameerasw <sameera.whoami@gmail.com>
2023-12-26 01:24:29 +0530

    initial release - Added API key options with default of Bing wallpapers,Added favorites folder selection, polished UI, optimized code, cleaned up quotes and more.

--- sketch/home.js
+++ sketch/home.js
@@ -6,11 +6,14 @@ let para = document.getElementById('title');
 let list = document.querySelector('ul');
 let container = document.querySelector('.container');
 let form = document.querySelector('form');
+let folderFieldset = document.querySelector('#folderField');
 const numbers = "0123456789";
 let interval = null;
 var provider;
 var backgroundPaused;
 var previousImage;
+var unsplashAPI;
+var pexelsAPI;
 
 let previousImageUrl = '';
 h1.style.opacity = 1;
@@ -40,9 +43,6 @@ function textanimate (word) {
     let iterations = 0;
     const duration = 250 / word.length;
 
-    //if the new word is the same as the old one, do nothing
-    // if (text.innerText === word) return;
-
     clearInterval(interval);
 
     interval = setInterval(() => {
@@ -71,21 +71,38 @@ function textanimate (word) {
 }
 
 function getImg(provider) {
-    console.log('got provider :' + provider);
+    // console.log('got provider :' + provider);
     //get image from provider
     if (provider === 'unsplash') {
         unsplash();
     } else if (provider === 'pexels') {
         pexels();
     } else {
-        unsplash();
+        bing();
+    }
+}
+
+function bing() {
+    //get daily image from bing
+    console.log('Using bing api');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US');
+    xhr.onload = function() {
+    if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var image = 'https://www.bing.com' + response.images[0].url;
+            //save image to local storage
+            chrome.storage.local.set({'image': image}, function() {
+                // console.log('saved image', image);
+            });
+        } 
     }
 }
 
 function unsplash() {
     console.log('Using unsplash api');
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.unsplash.com/photos/random?client_id=fcx0TMO2LYjLO2oAWgMfp4l2w1d6IXSNVW9ORaamji0');
+    xhr.open('GET', 'https://api.unsplash.com/photos/random?client_id=' + unsplashAPI);
     xhr.onload = function() {
     if (xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
@@ -101,7 +118,7 @@ function unsplash() {
 
 function pexels() {
     console.log('Using pexels api');
-    let api = 'INB1uHU6THskPr6OnPO7sqW7Yaf9QPFecSDsc8AHf0KT7Tp9zmWuK6uW';
+    let api = pexelsAPI;
     let url = `https://api.pexels.com/v1/curated?per_page=1&page=${Math.floor(Math.random() * 10) + 1}`;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -121,7 +138,7 @@ function pexels() {
 
 
 function randomWallpaper() {
-    console.log('randomWallpaper() got provider :' + provider);
+    // console.log('randomWallpaper() got provider :' + provider);
     //get image from local storage
     chrome.storage.local.get('image', function(data) {
         let imageUrl = data.image;
@@ -129,7 +146,7 @@ function randomWallpaper() {
         chrome.storage.local.set({'previousImage': imageUrl}, function() {
             // console.log('saved previousImage', imageUrl);
         } );
-        //set background image to the image from local storage(use orevios image if paused)
+        //set background image to the image from local storage(use previous image if paused)
         if (backgroundPaused) {
             background.style.backgroundImage = data.image;
         } else {
@@ -160,7 +177,7 @@ function time() {
 function bookmarkList() {
     let icons = [];
     //replace '1' with your prefered bookmark folder id
-    chrome.bookmarks.getSubTree('1', function(bookmarks) {
+    chrome.bookmarks.getSubTree(bookmarksFolderId, function(bookmarks) {
         // console.log(bookmarks);
         icons = fetchBookmarkIcons(bookmarks);
         process_bookmark(bookmarks,icons);
@@ -202,25 +219,23 @@ function welcomeAnimation() {
 function getProvider() {
     //ask user for provider (unsplash or pexels) or use default (unsplash)
     let providerGot = prompt('Enter provider (Default: unsplash)');
-    console.log(providerGot);
+    // console.log(providerGot);
     if (providerGot === 'unsplash') {
         chrome.storage.local.set({'provider': providerGot}, function() {
-            console.log('saved provider', providerGot);
         });
     } else if (providerGot === 'pexels') {
         chrome.storage.local.set({'provider': providerGot}, function() {
-            console.log('saved provider', providerGot);
         });
     } else {
-        chrome.storage.local.set({'provider': 'unsplash'}, function() {
-            console.log('saved default provider', 'unsplash');
+        chrome.storage.local.set({'provider': 'bing'}, function() {
         });
     }
+    console.log('saved provider', providerGot);
     return providerGot;
 }
 
 function timeRequest() {
-    //update time
+    //update time and assignment
     h1.innerText = time();
 }
 
@@ -236,15 +251,44 @@ form.addEventListener('submit', (e) => {
     let providerGot = form.elements.provider.value;
     if (providerGot === 'unsplash') {
         chrome.storage.local.set({'provider': providerGot}, function() {
-            console.log('saved provider', providerGot);
         });
     } else if (providerGot === 'pexels') {
         chrome.storage.local.set({'provider': providerGot}, function() {
-            console.log('saved provider', providerGot);
         });
     } else {
-        chrome.storage.local.set({'provider': 'unsplash'}, function() {
-            console.log('saved default provider', 'unsplash');
+        chrome.storage.local.set({'provider': 'bing'}, function() {
+        });
+    }
+    console.log('saved provider', providerGot);
+    //read and set bookmarks folder
+    let bookmarksFolderId = form.elements.folder.value;
+    chrome.storage.local.set({'bookmarksFolderId': bookmarksFolderId}, function() {
+        console.log('saved bookmarksFolderId', bookmarksFolderId);
+    });
+    //get and update unsplash api key
+    if (form.elements.unsplashKey.value) {
+        unsplashAPI = form.elements.unsplashKey.value;
+        chrome.storage.local.set({'unsplashAPI': unsplashAPI}, function() {
+            // console.log('saved unsplashAPI', unsplashAPI);
+        });
+    } else {
+        //clear api key if empty
+        unsplashAPI = '';
+        chrome.storage.local.set({'unsplashAPI': unsplashAPI}, function() {
+            // console.log('saved unsplashAPI', unsplashAPI);
+        });
+    }
+    //get and update pexels api key
+    if (form.elements.pexelsKey.value) {
+        pexelsAPI = form.elements.pexelsKey.value;
+        chrome.storage.local.set({'pexelsAPI': pexelsAPI}, function() {
+            // console.log('saved pexelsAPI', pexelsAPI);
+        });
+    } else {
+        //clear api key if empty
+        pexelsAPI = '';
+        chrome.storage.local.set({'pexelsAPI': pexelsAPI}, function() {
+            // console.log('saved pexelsAPI', pexelsAPI);
         });
     }
     //exit edit mode
@@ -258,12 +302,12 @@ form.addEventListener('reset', (e) => {
 
 function editMode() {
     //edit mode
-    // body.style.classList.add('edit');
-    //add or remove "edit" class to the body to toggle edit mode
     if (container.classList.contains('edit')) {
         container.classList.remove('edit');
         background.classList.remove('edit');
         h1.classList.remove('edit');
+        //update everything
+        bookmarkList();
     } else {
         container.classList.add('edit');
         background.classList.add('edit');
@@ -282,7 +326,29 @@ function editMode() {
         } else {
             document.querySelector('#pause').value = '⏸ Pause';
         }
-
+        //list bookmarks folder options
+        chrome.bookmarks.getTree(function(bookmarks) {
+            // console.log(bookmarks);
+            let options = bookmarks[0].children.map(bookmark => {
+                return `<option value="${bookmark.id}">${bookmark.title} , ${bookmark.id}</option>`;
+            }
+            ).join('');
+            folderFieldset.innerHTML = `<label for="folder">Bookmarks folder</label><select name="folder" id="folder">${options}</select>`;
+            form.elements.folder.value = bookmarksFolderId;
+            // console.log('bookmarksFolderId : ', bookmarksFolderId);
+        });
+        //get and set unsplash api key
+        chrome.storage.local.get('unsplashAPI', function(data) {
+            if (data.unsplashAPI) {
+                form.elements.unsplashKey.value = data.unsplashAPI;
+            }
+        });
+        //get and set pexels api key
+        chrome.storage.local.get('pexelsAPI', function(data) {
+            if (data.pexelsAPI) {
+                form.elements.pexelsKey.value = data.pexelsAPI;
+            }
+        });
     }
 }
 
@@ -292,9 +358,8 @@ function pauseWallpaper() {
         backgroundPaused = false;
         //save value to local storage
         chrome.storage.local.set({'backgroundPaused': backgroundPaused}, function() {
-            console.log('saved backgroundPaused', backgroundPaused);
+            // console.log('saved backgroundPaused', backgroundPaused);
         });
-        //set the text of the button to "pause"
         document.querySelector('#pause').value = 'pause';
         //changeimage
         randomWallpaper();
@@ -302,9 +367,8 @@ function pauseWallpaper() {
         backgroundPaused = true;
         //save value to local storage
         chrome.storage.local.set({'backgroundPaused': backgroundPaused}, function() {
-            console.log('saved backgroundPaused', backgroundPaused);
+            // console.log('saved backgroundPaused', backgroundPaused);
         });
-        //set the text of the button to "play"
         document.querySelector('#pause').value = 'play';
         //save the current background of the page to local storage
         chrome.storage.local.set({'image': background.style.backgroundImage}, function() {
@@ -313,10 +377,18 @@ function pauseWallpaper() {
     }
 }
 
-//enter edit mode when right click
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-    editMode();
+//enter edit mode with e key
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'e') {
+        editMode();
+    }
+});
+
+//refreshbookmarks with r key
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'r') {
+        bookmarkList();
+    }
 });
 
 
@@ -325,7 +397,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //try to read grid size from local storage or ask user for input
     try {
         chrome.storage.local.get('gridSizeX', function(data) {
-            console.log('grid size : ',data.gridSizeX);
+            // console.log('grid size : ',data.gridSizeX);
             let gridSizeX = data.gridSizeX;
             list.style.gridTemplateColumns = `repeat(${gridSizeX}, 1fr)`;
         });
@@ -336,7 +408,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //try to read provider from local storage or ask user for input
     try {
         chrome.storage.local.get('provider', function(data) {
-            console.log('provider from storage : ',data.provider);
+            // console.log('provider from storage : ',data.provider);
             provider = data.provider;
         });
     } catch (error) {
@@ -346,23 +418,57 @@ document.addEventListener('DOMContentLoaded', function() {
     //try to read backgroundPaused from local storage or set to false
     try {
         chrome.storage.local.get('backgroundPaused', function(data) {
-            console.log('backgroundPaused from storage : ',data.backgroundPaused);
+            // console.log('backgroundPaused from storage : ',data.backgroundPaused);
             backgroundPaused = data.backgroundPaused;
         });
     } catch (error) {
         backgroundPaused = false;
     }
 
-    bookmarkList();
+    //try to read bookmarksFolderId from local storage or set to '1'
+    chrome.storage.local.get('bookmarksFolderId', function(data) {
+        // console.log('bookmarksFolderId from storage : ',data.bookmarksFolderId);
+        if (data.bookmarksFolderId) {
+            bookmarksFolderId = data.bookmarksFolderId;
+        } else {
+            bookmarksFolderId = '1';
+        }
+        // console.log('bookmarksFolderId : ', bookmarksFolderId);
+    });
+
+    //try to read unsplashAPI from local storage or set to ''
+    chrome.storage.local.get('unsplashAPI', function(data) {
+        // console.log('unsplashAPI from storage : ',data.unsplashAPI);
+        if (data.unsplashAPI) {
+            unsplashAPI = data.unsplashAPI;
+        } else {
+            unsplashAPI = '';
+        }
+        // console.log('unsplashAPI : ', unsplashAPI);
+    });
+
+    //try to read pexelsAPI from local storage or set to ''
+    chrome.storage.local.get('pexelsAPI', function(data) {
+        // console.log('pexelsAPI from storage : ',data.pexelsAPI);
+        if (data.pexelsAPI) {
+            pexelsAPI = data.pexelsAPI;
+        } else {
+            pexelsAPI = '';
+        }
+        // console.log('pexelsAPI : ', pexelsAPI);
+    });
+
+    setTimeout(
+        bookmarkList,
+        500
+    )
     setTimeout(
         randomWallpaper,
         1000
     )
     setTimeout(welcomeAnimation, 500);
     
-    // console.log('randomWallpaper()');
     //update time once 5 seconds
-    // textanimate('00:00');
     textanimate(time());
     setInterval(timeRequest, 5000);
     
